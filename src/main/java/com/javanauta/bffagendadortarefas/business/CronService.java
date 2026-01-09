65d779119be10c82719a58f39fa2a2e2ca5ef644
hkojim <eng.koji@gmail.com>
2026-01-09 16:26:50 -0300

    adicionado cron

---
+++ src/main/java/com/javanauta/bffagendadortarefas/business/CronService.java
@@ -0,0 +1,59 @@
+package com.javanauta.bffagendadortarefas.business;
+
+import com.javanauta.bffagendadortarefas.business.dto.in.LoginRequestDTO;
+import com.javanauta.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.javanauta.bffagendadortarefas.business.enums.StatusNotificacaoEnum;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CronService {
+
+    private final TarefaService tarefaService;
+    private final EmailService emailService;
+    private final UsuarioService usuarioService;
+
+    @Value("${usuario.email}")
+    private String email;
+
+    @Value("${usuario.senha}")
+    private String senha;
+
+    @Scheduled(cron=("${cron.horario}"))
+    public void buscaTarefasProximaHora(){
+        String token = login(converterParaRequestDTO());
+        log.info("Iniciada a busca de tarefas");
+        LocalDateTime horaAtual = LocalDateTime.now(); //Localdate.now (hora atual)
+        LocalDateTime horaFutura = LocalDateTime.now().plusHours(1);
+        //Qualquer tarefa que fique entre hora atual - e a hora futura +1
+        //Se agora é 22h - qualquer tarefa entre 22h e 23h
+
+        List<TarefasDTOResponse> listaTarefas = tarefaService.buscaTarefasAgendadasPorPeriodo(horaAtual, horaFutura,token);
+        log.info("Tarefas encontradas" + listaTarefas);
+        listaTarefas.forEach(tarefa -> {emailService.enviaEmail(tarefa);
+            log.info("Email enviado para o usuário" +tarefa.getId());
+            tarefaService.alteraStatus(StatusNotificacaoEnum.NOTIFICADO, tarefa.getId(),
+                    token);
+        });
+        log.info("Finalizada a busca e notificação de tarefas");
+    }
+
+    public String login(LoginRequestDTO dto){
+        return usuarioService.loginUsuario(dto);
+    }
+
+    public LoginRequestDTO converterParaRequestDTO(){
+        return LoginRequestDTO.builder()
+                .email(email)
+                .senha(senha)
+                .build();
+    }
+}
